6fd60b31914fff0c9760b4fb838cfd467e1db744
Lucas Góes <74834174+lucas641@users.noreply.github.com>
2022-05-26 22:03:43 -0300

    login

--- App.jsx
+++ App.jsx
@@ -1,12 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Text, View, TextInput, SafeAreaView, TouchableOpacity } from 'react-native';
 import { styles } from './styles/global-style';
+import { url } from './config/config.json';
 
 export default function App() {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [temp, setTemp] = useState("");
+
+  const handleChangeEmail = email => setEmail(email);
+  const handleChangeSenha = senha => setSenha(senha);
+
+  const sendForm = async () => {
+    const request = await fetch(`${url}login`, {
+      method: "POST",
+      headers: { "Content-type": "application/json;charset=UTF-8" },
+      body: JSON.stringify({ email, senha })
+    });
+    const res = await request.json();
+    setTemp(String(res));
+  }
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Olá mundo!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.text}>Login SM Estoque</Text>
+      </View>
+
+      <SafeAreaView>
+        <TextInput
+          style={styles.input}
+          onChangeText={handleChangeEmail}
+          value={email}
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={handleChangeSenha}
+          value={senha}
+          placeholder="Senha..."
+          maxLength={30}
+          secureTextEntry={true}
+        />
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={sendForm}
+        >
+          <Text>Login</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+      <Text style={styles.text}>{temp}</Text>
     </View>
   );
 }
--- styles/global-style.jsx
+++ styles/global-style.jsx
@@ -7,4 +7,21 @@ export const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text: {
+    fontSize: 23
+  },
+
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10
+  },
 });
